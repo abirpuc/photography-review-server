0dc2c08c759c829b1223465219a4d1b8e88d499b
abirpuc <mhabirbd2016@gmail.com>
2022-11-21 22:53:43 +0600

    loade all services data

--- index.js
+++ index.js
@@ -10,21 +10,32 @@ app.use(express.json());
 
 require('dotenv').config();
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.k6rknfb.mongodb.net/?retryWrites=true&w=majority`;
-
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run(){
+    try{
+        const serverCollection = client.db('PhotographyReview').collection('services')
+
+        app.get('/services', async(req,res) =>{
+            const query = {}
+            const cursor = serverCollection.find()
+            const service = await cursor.toArray()
+            res.send(service)
+        })
+    }
+    finally{
+
+    }
+}
+
+run().catch(err =>console.log(err))
 
 
-app.get('/', (req,res) =>{
+app.get('/', (req, res) => {
     res.send('Review server is running!!')
 })
 
-app.listen(port, (req,res) =>{
+app.listen(port, (req, res) => {
     console.log(`Review server is running on ${port}`);
 })
