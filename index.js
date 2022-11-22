b0e43cec9f6a8eadd6e4f827e52dbd20202f7e30
abirpuc <mhabirbd2016@gmail.com>
2022-11-22 11:45:03 +0600

    view sing service details

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const cors = require('cors')
 const port = process.env.PORT || 5000
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 
 app.use(cors());
@@ -15,12 +15,19 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 async function run(){
     try{
-        const serverCollection = client.db('PhotographyReview').collection('services')
+        const serviceCollection = client.db('PhotographyReview').collection('services')
 
         app.get('/services', async(req,res) =>{
             const query = {}
-            const cursor = serverCollection.find()
+            const cursor = serviceCollection.find()
             const service = await cursor.toArray()
+            res.send(service)
+        })
+
+        app.get('/services/:id', async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)}
+            const service = await serviceCollection.findOne(query)
             res.send(service)
         })
     }
