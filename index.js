87403842e02d0472d8d5c7318f6a9f03107ea504
abirpuc <mhabirbd2016@gmail.com>
2022-11-23 19:30:49 +0600

    add a new service

--- index.js
+++ index.js
@@ -30,6 +30,12 @@ async function run(){
             const service = await serviceCollection.findOne(query)
             res.send(service)
         })
+
+        app.post('/services', async(req,res)=>{
+            const service = req.body;
+            const result = await serviceCollection.insertOne(service)
+            res.send(service)
+        })
     }
     finally{
 
